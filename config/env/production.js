8a121ebad9468b57c151747b8013f7d736350cff
rivolela <contato@pickoout.com.br>
2016-09-20 16:42:45 +0100

    add env > production.js

---
+++ config/env/production.js
@@ -0,0 +1,5 @@
+module.exports = {
+// Development configuration options
+	db:' mongodb://contato@pickoout.com.br:Rovel@1976@ds035776.mlab.com:35776/heroku_5lt8spw4',
+	sessionSecret:'productSessionSecret'
+};
