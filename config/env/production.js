e91ac81e16dc47ff976b56d24561de5fb04792c6
rivolela <contato@pickoout.com.br>
2016-09-20 17:09:09 +0100

    fix url mongo db from production.js

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,5 @@
 module.exports = {
 // Development configuration options
-	db:' mongodb://contato@pickoout.com.br:Rovel@1976@ds035776.mlab.com:35776/heroku_5lt8spw4',
+	db:'mongodb://contato@pickoout.com.br:Rovel@1976@ds035776.mlab.com:35776/heroku_5lt8spw4',
 	sessionSecret:'productSessionSecret'
 };
