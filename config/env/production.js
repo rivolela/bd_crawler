e4844c239d4e540251affc7f3e6f44a68de134d7
rivolela <contato@pickoout.com.br>
2016-10-25 14:13:38 +0100

    test ricardo eletro br

--- config/env/production.js
+++ config/env/production.js
@@ -16,7 +16,7 @@ module.exports = {
 
 	// schedule reviews jobs
 	walmart_schedule:'30 15 * * *',
-	ricardo_eletro_schedule:'10 13 * * *',
+	ricardo_eletro_schedule:'20 13 * * *',
 	lojas_colombo_schedule:'30 17 * * *',
 
 	// urls crawler
