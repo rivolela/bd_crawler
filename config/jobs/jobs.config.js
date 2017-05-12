fcc005e3fecda301e36d4b0be9e77502d6d9822f
rivolela <contato@pickoout.com.br>
2017-05-12 14:59:28 +0100

    test update product

--- config/jobs/jobs.config.js
+++ config/jobs/jobs.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
 	//schedule reviews job
-	all_schedule:'41 14 * * *',
+	all_schedule:'5 13 * * *',
 	walmart_schedule:'0 20 * * *',
 	ricardo_eletro_schedule:'40 18 * * *',
 	lojas_colombo_schedule:'25 17 * * *',
