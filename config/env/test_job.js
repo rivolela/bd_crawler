48b5498eb1aef56ef9b8092537350f1c5c20db50
rivolela <contato@pickoout.com.br>
2019-06-18 10:01:04 +0100

    add /config/env/ in .gitignore file

--- config/env/test_job.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	// Development configuration options
-	db:'mongodb://127.0.0.1:27017/bd_test_job',
-	sessionSecret:'developmentSessionSecret',
-	connectid:'43EEF0445509C7205827',
-	timeRequest:2000,
-};
