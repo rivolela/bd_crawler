ee4c2b04c26b4fbfc123712efdf70986c9d57b15
rivolela <contato@pickoout.com.br>
2019-06-18 10:03:48 +0100

    remove data base information from express.js

--- config/express.js
+++ config/express.js
@@ -32,23 +32,7 @@ module.exports = function () {
 		secret:config.sessionSecret
 	}));
 
-	// example proxy
-	// app.use('/proxy', proxy('http://rivolela:Rovel@1976@proxy-br1.vpnsecure.me:8080', {
- //  		forwardPath: function(req, res) {
- //  			console.log(require('url').parse(req.url).path);
- //    		return require('url').parse(req.url).path;
- // 		}
-	// }));
 
-
-	// example : how to use cookie informations
-	// app.use(function (req, res, next) {
- //  		// Update views 
- //  		req.session.views = (req.session.views || 0) + 1;
- 
- //  		// Write response 
- //  		res.end(req.session.views + ' views');
-	// });
  
 	require('../app/routes/walmart.server.routes.js')(app);
 
